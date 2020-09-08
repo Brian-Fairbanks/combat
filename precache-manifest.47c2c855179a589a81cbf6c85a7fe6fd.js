2dfaed7fa5f63e0de43487eef0af9d9edf72be36
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-09-08 18:39:22 -0500

    Updates

---
+++ precache-manifest.47c2c855179a589a81cbf6c85a7fe6fd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d93b4adeef11791252ba835c2e6bdbc",
+    "url": "/combat/index.html"
+  },
+  {
+    "revision": "b8d2a031b4257081bfdb",
+    "url": "/combat/static/css/main.48aa3bf8.chunk.css"
+  },
+  {
+    "revision": "c92d61e3124b79531ec7",
+    "url": "/combat/static/js/2.0de4a867.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/combat/static/js/2.0de4a867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8d2a031b4257081bfdb",
+    "url": "/combat/static/js/main.b3a94fee.chunk.js"
+  },
+  {
+    "revision": "6b0c94adf80747060e84",
+    "url": "/combat/static/js/runtime-main.f165c3aa.js"
+  },
+  {
+    "revision": "d4efd15c68ae260d7784631ad02e196a",
+    "url": "/combat/static/media/dragonHead.d4efd15c.svg"
+  },
+  {
+    "revision": "d30bbb60fd958066dcf718a93d616d46",
+    "url": "/combat/static/media/dragonTail.d30bbb60.svg"
+  },
+  {
+    "revision": "d788e64a8dcd2f87ffc9ecc878407934",
+    "url": "/combat/static/media/statborder.d788e64a.svg"
+  },
+  {
+    "revision": "12ec41d8346a904611eff20f60cae895",
+    "url": "/combat/static/media/styledBorder.12ec41d8.svg"
+  },
+  {
+    "revision": "5f410990db24de2ab1eb61b0bc0ab601",
+    "url": "/combat/static/media/styledBorder2.5f410990.svg"
+  },
+  {
+    "revision": "edf6f5b5292ac8eef1e0722fde918ee2",
+    "url": "/combat/static/media/styledBorder3.edf6f5b5.svg"
+  }
+]);
